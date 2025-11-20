28cb1039d8c910d47c6472c584369cb7f7dede00
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-11-20 14:37:08 +0800

    fix

--- lambda.tf
+++ lambda.tf
@@ -1,18 +1,18 @@
 locals {
   lambda_program_name = "kg_monitoring_check_web"
-  output_path         = "${path.module}/lambda_check_web/lambda_function_payload.zip"
 }
 
 data "archive_file" "check_web_lambda" {
   type        = "zip"
   source_file = "${path.module}/lambda_check_web/lambda_function.py"
-  output_path = local.output_path
+  output_path = "${path.module}/lambda_check_web/lambda_function_payload.zip"
 }
 
 resource "aws_lambda_function" "check_web" {
   architectures    = ["arm64"]
   description      = "Performs a periodic check of the given site, erroring out on test failure."
-  filename         = local.output_path
+  filename         = data.archive_file.check_web_lambda.output_path
+  source_code_hash = data.archive_file.check_web_lambda.output_base64sha256
   function_name    = "arn:aws:lambda:${var.region}:${var.kism_account_id}:function:${local.lambda_program_name}"
   handler          = "lambda_function.lambda_handler"
   memory_size      = 128
@@ -20,7 +20,6 @@ resource "aws_lambda_function" "check_web" {
   role             = aws_iam_role.check_web.arn
   runtime          = "python3.13"
   timeout          = 10
-  source_code_hash = data.archive_file.check_web_lambda.output_base64sha256
   environment {
     variables = {
       site = "https://grafana.kierangee.au/api/health"
