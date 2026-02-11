22a7270a077b5d4ffafa43be85a74d96ba7d22c0
Kieran Gee <kieran.lost.the.game@gmail.com>
2026-02-11 15:25:35 +0800

    logging

--- lambda_check_web/lambda_function.py
+++ lambda_check_web/lambda_function.py
@@ -1,11 +1,20 @@
 import os
 import urllib3
-import logging 
+import logging
 
+
+SITE = os.environ[
+    "site"
+]  # URL of the site to check, stored in the site environment variable
+
+# We don't use the real logger yet since we want to be able to diagnose import issues
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-SITE = os.environ['site']  # URL of the site to check, stored in the site environment variable
+# Configure logger to only show messages without level/timestamp/request ID
+for log_handler in logger.handlers:
+    log_handler.setFormatter(logging.Formatter("[%(levelname)s] %(message)s"))
+
 
 def lambda_handler(event, context):
     if not SITE:
@@ -13,16 +22,13 @@ def lambda_handler(event, context):
 
     http = urllib3.PoolManager()
 
-    response = http.request('GET', SITE, timeout=10.0)
+    response = http.request("GET", SITE, timeout=10.0)
 
     if response.status == 200:
-        msg = f'URL {SITE} returned {response.status} status code'
+        msg = f"URL {SITE} returned {response.status} status code"
         logger.info(msg)
-        return {
-            'statusCode': 200,
-            'body': msg
-        }
-        
+        return {"statusCode": 200, "body": msg}
+
     else:
         msg = f"{SITE} website is broken? Code: {response.status}"
         logger.error(msg)
